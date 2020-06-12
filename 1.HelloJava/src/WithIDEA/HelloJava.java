6e270b34b7b61b0fbe3ac383b46e97ba69f823d3
QuentinHsu <xuquentinyang@gmail.com>
2020-06-12 14:57:25 +0800

    perfect(HelloJava): perfect Java basis content, and test

--- 1.HelloJava/src/WithIDEA/HelloJava.java
+++ 1.HelloJava/src/WithIDEA/HelloJava.java
@@ -1,7 +1,24 @@
 package WithIDEA;
 
+/*类名要求：
+- 类名必须以英文字母开头，后接字母，数字和下划线的组合
+- 习惯以大写字母开头
+于是 我初次写的类名，貌似并不符合 Java 开发命名习惯
+
+类名 务必与 文件名 完全相同，不然会编译报错。
+ */
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello Java! Hello IntelliJ IDEA!");
+        // 向屏幕输出文本：
+        System.out.println("Hello world! Hello Java!");
     }
-}
+}// class 定义结束
+/* 各标识符解释：
+
+    public 访问修饰符，表示该 class 是公开的
+    static 方法修饰符，表示是静态方法
+    void   返回值，表示是没有任何返回值
+    main   方法名
+
+    在 class 内部可以定义若干方法（method）
+ */
